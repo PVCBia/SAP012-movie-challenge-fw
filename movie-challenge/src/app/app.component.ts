5db25e2add5ef525579f5f0809b13ce0496aae2d
PVCBia <pvcbeatriz@gmail.com>
2024-05-27 11:20:32 -0300

    inserção de environment token_api

--- movie-challenge/src/app/app.component.ts
+++ movie-challenge/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { enviroment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-challenge';
+  apiUrl: string | undefined;
+
+  constructor() {
+    this.apiUrl = enviroment.TOKEN_API;
+    console.log(this.apiUrl);
+  }
 }
